075d2c87eab88e500d7b3037ab09ae30a63f72d6
Josh Riesenbach <josh.riesenbach@gmail.com>
2016-02-15 13:49:13 -0700

    Changed buttons to inputs and set clock to use inputs to calculate wait time

--- public/client/app.js
+++ public/client/app.js
@@ -5,11 +5,20 @@ app.controller("info-controller", function ($scope) {
   $scope.location = "riesenable.io";
 });
 
-app.controller("clock-controller", function ($scope) {
+app.controller("clock-controller", function ($scope, $timeout) {
+  
   var updateTime = function () {
-    $scope.time = new Date();
-    console.log("updated");
-    setTimeout(updateTime, 1000);
+    $scope.time = Date.now() + $scope.wait.ms();
+    console.log("Current time: ", $scope.wait);
+    $timeout(updateTime, 100);
+  };
+  
+  $scope.wait = {
+    hours: 0,
+    minutes: 0,
+    ms: function () {
+      return (this.hours * 60 + this.minutes) * 60 * 1000;
+    }
   };
   
   updateTime();
